fde6fabe0f6c4d62d487851985c58717a1bbbed9
gold_roger <1rn21cs016.akash@gmail.com>
2023-06-04 22:08:11 +0530

    Added delta time for smoother rendering

--- Dasher.cpp
+++ Dasher.cpp
@@ -9,7 +9,7 @@ int main()
     //rectangel dimensions
     // const int rec_width{50};
     // const int rec_height{80};
-    const int gravity{1};// this is p/f /f
+    const int gravity{3000};// this is in pixels/s /s
 
     Texture2D scrafy=LoadTexture("D:/GameDev/DapperDasher/textures/scarfy.png"); // this is a compound data type essentially making an object of the class texture2D and we can access all the functions  that come with the class and use it on the object itself
     Rectangle scrafyrec;//compound datatype
@@ -22,18 +22,21 @@ int main()
     scrafyPos.x=width/2-scrafyrec.width/2;// this splits the scrafy frame into 2 equal half and thus the image itself appears at the center of the screen
     scrafyPos.y=height-scrafyrec.height;// since we treate the image as a upper left point
 
+
     //int posY{height-rec_height};//here we treat the rectangle as a single point on the top left corner of the rectangel of that width so height of the window would place the point at the bottom of the canvas and subtracting the height od the rectangel will place the actual rectangle on the bottn
     int velocity{0};
     //is the rectangle in the air
     bool isInAir{1};
     //jump velocity
-    const int jumpVel{-22};
+    const int jumpVel{-600};// px/s
 
     SetTargetFPS(60); // this tells the system that it should display 60 complete frame per second to  the screen and well if we dont use this function then the sprite will achieve the highest possible frame rate 
     while(!WindowShouldClose())//since window should close will return true iff the x or the escape buttons are pressed
     {// the body of the while loop executes every frame
         BeginDrawing();
         ClearBackground(WHITE);
+        const float dT{GetFrameTime()};// this is the time duration between each frame and as the acutal number of frames increases then the dT reduces and viceversa
+
         if(scrafyPos.y>=height-scrafyrec.height)// this is the ground check condition
         {
             //rectangle is on the ground
@@ -43,7 +46,7 @@ int main()
         else
         {
             //rectangle is in the air and need to come back due to gravity
-            velocity+=gravity;
+            velocity+=gravity * dT;
             isInAir=true;
         }
         if(IsKeyPressed(KEY_SPACE)&&isInAir==false)// this means that ti will only jump if the rectangle is on the ground
@@ -51,7 +54,7 @@ int main()
             velocity += jumpVel;// always remember our convention that we need a negative value if we want our sprite to move upward from the bottom of the canvas this makes it jump indirectly
         }
         
-        scrafyPos.y+=velocity;// this is what actually moves the rectangle
+        scrafyPos.y+=velocity * dT;// this is what actually moves the rectangle we multiply the position with dT because the units change from pizels per sec to pixels per frame
 
         DrawTextureRec(scrafy,scrafyrec,scrafyPos,WHITE);
 
