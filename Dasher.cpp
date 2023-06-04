00e8d3ed515dccdbfa5636f2515dd4ef0cdf9b3b
gold_roger <1rn21cs016.akash@gmail.com>
2023-06-04 18:50:44 +0530

    solved double jumping problem

--- Dasher.cpp
+++ Dasher.cpp
@@ -12,27 +12,32 @@ int main()
     const int gravity{1};// this is p/f /f
     int posY{height-rec_height};//here we treat the rectangle as a single point on the top left corner of the rectangel of that width so height of the window would place the point at the bottom of the canvas and subtracting the height od the rectangel will place the actual rectangle on the bottn
     int velocity{0};
+    //is the rectangle in the air
+    bool isInAir{1};
+    //jump velocity
+    const int jumpVel{-22};
 
     SetTargetFPS(60);
     while(!WindowShouldClose())//since window should close will return true iff the x or the escape buttons are pressed
     {// the body of the while loop executes every frame
         BeginDrawing();
         ClearBackground(WHITE);
-        if(posY>=height-rec_height)
+        if(posY>=height-rec_height)// this is the ground check condition
         {
             //rectangle is on the ground
+            isInAir=false;
             velocity=0;
         }
         else
         {
             //rectangle is in the air and need to come back due to gravity
             velocity+=gravity;
+            isInAir=true;
         }
-        if(IsKeyPressed(KEY_SPACE))
+        if(IsKeyPressed(KEY_SPACE)&&isInAir==false)// this means that ti will only jump if the rectangle is on the ground
         {
-            velocity-=10;// always remember our convention that we need a negative value if we want our sprite to move upward from the bottom of the canvas
+            velocity += jumpVel;// always remember our convention that we need a negative value if we want our sprite to move upward from the bottom of the canvas this makes it jump indirectly
         }
-        
         
         posY+=velocity;// this is what actually moves the rectangle
         DrawRectangle(width/2,posY,rec_width,rec_height,BLUE);
